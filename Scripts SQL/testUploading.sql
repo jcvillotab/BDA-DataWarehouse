5c559aae615459c3f4fd7f2f7acf6c3a5badcb26
jcvillotab <jcvillotab@unal.edu.co>
2020-09-26 22:55:12 -0500

    Segunda version- fata crear algunas dimensiones. Dim Fecha cliente y empresa ya estan

--- Scripts SQL/testUploading.sql
+++ Scripts SQL/testUploading.sql
@@ -1,11 +1,74 @@
 Create database testPython;
 use testPython;
 
-Create table Empresa(
-	empresaId int,
-    nombreEmpresa varchar(60),
-    region varchar(60)
+Create table Dim_Empresa(
+	empresaId 		int PRIMARY KEY ,
+    nombreEmpresa 	varchar(60),
+    region 			varchar(60),
+    idRegion 		int(4)
 );
 
-	
+Create table Dim_Cliente(
+    idCliente   int AUTO_INCREMENT primary key,
+    nombre 		varchar(60),
+    razonSocial varchar(60),
+    documento 	varchar(15),
+    direccion 	varchar(60),
+    telefono 	varchar(15)
+);
 
+CREATE TABLE Dim_Fecha (
+	id                    INTEGER PRIMARY KEY,
+	db_date               DATE NOT NULL,
+	año                   INTEGER NOT NULL,
+	mes                   INTEGER NOT NULL, 
+	nombre_del_mes        VARCHAR(9) NOT NULL, 
+	dia                   INTEGER NOT NULL, 
+	trimestre             INTEGER NOT NULL, 
+	semana                INTEGER NOT NULL, 
+	nombre_dia            VARCHAR(9) NOT NULL, 
+	dia_semana            VARCHAR(9) NOT NULL,
+	dia_año               VARCHAR(9) not NULL,
+	UNIQUE td_ymd_idx (año,mes,dia),
+	UNIQUE td_dbdate_idx (db_date)
+
+) Engine=MyISAM;
+
+DROP PROCEDURE IF EXISTS poblar_Dim_Fecha;
+DELIMITER //
+CREATE PROCEDURE poblar_Dim_Fecha(IN startdate DATE,IN stopdate DATE)
+BEGIN
+    DECLARE currentdate DATE;
+    SET currentdate = startdate;
+    WHILE currentdate <= stopdate DO
+        INSERT INTO Dim_Fecha VALUES (
+            date_format(currentdate, "%Y%m%d"),
+            currentdate,
+            YEAR(currentdate),
+            MONTH(currentdate),
+			monthname(currentdate),
+            DAY(currentdate),
+            QUARTER(currentdate),
+            WEEKOFYEAR(currentdate),
+            dayname(currentdate),
+            DAYOFWEEK(currentdate),
+            dayofyear(currentdate)
+            );
+        SET currentdate = ADDDATE(currentdate,INTERVAL 1 DAY);
+    END WHILE;
+END
+//
+DELIMITER ;
+
+TRUNCATE TABLE Dim_Fecha;
+CALL poblar_Dim_Fecha('2020-01-01','2020-12-31');
+OPTIMIZE TABLE Dim_Fecha;
+#select * from Dim_Fecha;
+
+#select * from Dim_Cliente;
+#drop table Dim_Cliente
+
+
+#drop table Dim_Empresa
+#select * from Dim_Empresa
+#truncate table Dim_Empresa
--- test.py
+++ test.py
@@ -30,30 +30,69 @@ def executeInsertStatement(connection, statement, values):
     cursor.close()
 
 
-def insertEmpresas(listE):
-    absPath = os.path.join(os.path.dirname(sys.argv[0]), "Datos.xlsx")
-    workbook = opx.load_workbook(absPath)
+def insertEmpresas(listE, workbook):
     sheet = workbook.active
     myDB = createConnection()
 
-    for i in range(2, 15):
-        cValue = sheet["C"+str(i)].value
+    
+    for i in range(2, 100):
+        cValue = sheet["D"+str(i)].value
         idEmpresa = re.search('([0-9]+)', cValue).group()
-        nombreEmpresa = cValue[re.search('([0-9]+)', cValue).span()[1] + 3 :]
-        #print(idEmpresa)
-        #print(nombreEmpresa)
-        bValue= sheet["B"+str(i)].value
-        region = bValue.split(" - ")[1]
-        idRegion = bValue.split(" - ")[0]
+        if idEmpresa not in listE:
+            nombreEmpresa = cValue[re.search('([0-9]+)', cValue).span()[1] + 3 :]
+            bValue= sheet["C"+str(i)].value
+            region = bValue.split(" - ")[1]
+            idRegion = bValue.split(" - ")[0]
 
-        statement = "INSERT INTO empresa values (%s,%s, %s)"
-        val = (idEmpresa, nombreEmpresa, region)
+            statement = "INSERT INTO Dim_empresa values (%s,%s, %s, %s)"
+            val = (idEmpresa, nombreEmpresa, region, idRegion)
 
+            executeInsertStatement(myDB, statement,val)
+            listE.append(idEmpresa)
         
+
+    myDB.close()
+
+def insertCliente(listClient, workbook):
+    sheet = workbook.active
+    myDB = createConnection()
+    for i in range(2, 100):
+        nombre = sheet["G"+str(i)].value
+        razonSocial = sheet["H"+str(i)].value
+        documento = sheet["I"+str(i)].value
+        telefono = sheet["K"+str(i)].value
+        jValue = sheet["J"+str(i)].value
+
+        if "CARRERA" in jValue:
+            jValue = jValue.replace("CARRERA", "CRA")
+        elif "CALLE" in jValue:
+            jValue = jValue.replace("CALLE", "CLL")
+        elif "AVENIDA" in jValue:
+            jValue = jValue.replace("AVENIDA", "AV")
+        elif "DIAGONAL" in jValue:
+            jValue = jValue.replace("DIAGONAL", "DIAG")
+
+        if ")" in telefono:
+            telefono = telefono.split(")")[1]
+        if "-" in telefono:
+            telefono = telefono.split("-")[0]
+        if "/" in telefono:
+            telefono = telefono.split("/")[0]
+        
+        statement = "INSERT INTO Dim_Cliente (nombre, razonSocial, documento, direccion, telefono) values (%s,%s, %s, %s, %s)"
+        val = (nombre, razonSocial, documento, jValue, telefono)
         executeInsertStatement(myDB, statement,val)
     myDB.close()
-    
-insertEmpresas({})
+
+
+
+absPath = os.path.join(os.path.dirname(sys.argv[0]), "Datos.xlsx")
+workbook = opx.load_workbook(absPath)
+listaEmpresas = []
+diccClientes = {}
+insertEmpresas(listaEmpresas, workbook)
+insertCliente(diccClientes, workbook)
+
 
 
 
